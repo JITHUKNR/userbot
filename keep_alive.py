544d84ed5de08fd14db6195b2c66c4936899160c
JITHUKNR <96571770+JITHUKNR@users.noreply.github.com>
2026-07-09 03:48:06 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "ബോട്ട് വിജയകരമായി റൺ ചെയ്യുന്നുണ്ട്..."
+    return "Bot is alive and running 24/7!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render നൽകുന്ന പോർട്ട് (Port) തനിയെ കണ്ടുപിടിക്കാൻ
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
