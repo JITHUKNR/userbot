824dcaa5c3fa6a662d278879cb2458bbdb36ec08
JITHUKNR <96571770+JITHUKNR@users.noreply.github.com>
2026-09-17 11:27:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Config:
     API_ID = 34818480
     API_HASH = "a82911ecbf23a7ead187d410a34cf47a"
     BOT_TOKEN = "8838643631:AAEldj1tMFZQNZQ4fWHdd4mcx-w_QLEZkB4"
+    MONGO_URL = "mongodb+srv://oggyefx95_db_user:eO12QXv7usNzVbCI@cluster0.vkveveb.mongodb.net/your_database_name?retryWrites=true&w=majority&appName=Cluster0"
